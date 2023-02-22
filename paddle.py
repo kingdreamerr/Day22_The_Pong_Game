703e60af678bde4e1d49c19c07b06f919738e0cb
kingdreamerr <jackmvula458@gmail.com>
2023-02-22 22:56:13 +0200

    Add methods to the paddle class to enable players to move the paddles on both side of the screen

--- paddle.py
+++ paddle.py
@@ -12,3 +12,12 @@ class Paddle(Turtle):
         self.penup()
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.goto(position)
+
+    
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
