bc300bdc3997abd469dbcdff7b23de65876b5717
kingdreamerr <jackmvula458@gmail.com>
2023-02-22 22:58:30 +0200

    Add methods to move the ball and detect collisions with the top and bottom parts of the screen and the paddles

--- ball.py
+++ ball.py
@@ -14,3 +14,21 @@ class Ball(Turtle):
         self.x_move = 10
         self.y_move = 10
         self.ball_speed = 0.1
+
+    def move_ball(self):
+        new_y = self.ycor() + self.y_move
+        new_x = self.xcor() + self.x_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.ball_speed *= 0.9
+
+    def refresh(self):
+        self.goto(0, 0)
+        self.ball_speed = 0.1
+        self.x_move *= -1
+        self.move_ball()
