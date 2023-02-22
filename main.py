77e9f8c84be1b1f2eb9d2ea5e4d14b66d9f12312
kingdreamerr <jackmvula458@gmail.com>
2023-02-22 23:02:27 +0200

    Import the needed class into main.py and initialize them

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
+screen.screensize(canvwidth=800, canvheight=600)
+screen.tracer(0)
+screen.title("Pong")
+board = Scoreboard()
 
-screen.exitonclick()
+ball = Ball()
+# paddle 1
+paddle = Paddle((450, 0))
+
+# paddle 2
+paddle2 = Paddle((-450, 0))
+
+game_on = True
+
+screen.listen()
