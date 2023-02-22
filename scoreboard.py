1e026486c2d97728468024e76a46ac6b4c6dc7a7
kingdreamerr <jackmvula458@gmail.com>
2023-02-22 23:01:29 +0200

    Add methods to update the score

--- scoreboard.py
+++ scoreboard.py
@@ -10,3 +10,19 @@ class Scoreboard(Turtle):
         self.left = 0
         self.right = 0
         self.update_score()
+
+    def update_score(self):
+        self.goto(-100, 200)
+        self.write(self.left, align='center', font=('Courier', 80, 'normal'))
+        self.goto(100, 200)
+        self.write(self.right, align='center', font=('Courier', 80, 'normal'))
+
+    def l_score(self):
+        self.left += 1
+        self.clear()
+        self.update_score()
+
+    def r_score(self):
+        self.right += 1
+        self.clear()
+        self.update_score()
