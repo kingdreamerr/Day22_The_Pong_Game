a7ad34fb54353461a680c963a39056621a12bd81
kingdreamerr <jackmvula458@gmail.com>
2023-02-22 23:03:41 +0200

    Add conditions to check if the ball collides with the top or bottom or one of the paddles

--- main.py
+++ main.py
@@ -21,3 +21,33 @@ paddle2 = Paddle((-450, 0))
 game_on = True
 
 screen.listen()
+
+screen.onkeypress(paddle.go_up, "Up")
+screen.onkeypress(paddle.go_down, "Down")
+
+screen.onkeypress(paddle2.go_up, "w")
+screen.onkeypress(paddle2.go_down, "s")
+
+while game_on:
+    time.sleep(ball.ball_speed)
+    screen.update()
+    ball.move_ball()
+
+    if ball.ycor() > 365 or ball.ycor() < -365:
+        ball.bounce_y()
+
+#     collision with the paddle
+    if ball.distance(paddle) < 50 and ball.xcor() > 420 or ball.distance(paddle2) < 50 and ball.xcor() < -420:
+        ball.bounce_x()
+
+#     detect score
+    if ball.xcor() > 460:
+        ball.refresh()
+        board.l_score()
+
+    if ball.xcor() < -460:
+        ball.refresh()
+        board.r_score()
+
+
+screen.exitonclick()
